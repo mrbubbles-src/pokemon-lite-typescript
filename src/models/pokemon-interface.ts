6b9868fe9dabcdd1d7c6203c9bcdaa609db419ab
mrbubbles-src <manuel_fahrenholz@web.de>
2024-04-14 22:01:14 +0200

    Refactor PokemonInterface to include AttackerPokemonInterface and update method signatures

--- src/models/pokemon-interface.ts
+++ src/models/pokemon-interface.ts
@@ -1,7 +1,30 @@
+import { AttackSkillInterface } from "./attack-skill-interface";
+export interface AttackerPokemonInterface {
+    name: string;
+    health: number;
+    maxHealth: number;
+    magic: number;
+    maxMagic: number;
+    skills: AttackSkillInterface[];
+}
 export interface PokemonInterface {
     showStatus(): string;
-    learnAttackSkill(skill: string): string;
-    attack(skillIndex: number, attackedPokemon: string): string;
+    learnAttackSkill(skill: AttackSkillInterface): string;
+    attack(skillIndex: number, attackedPokemon: AttackerPokemonInterface): void;
     healthPotion(): string;
     magicPotion(): string;
+    performAttack(
+        skillName: AttackSkillInterface,
+        attackedPokemon: AttackerPokemonInterface
+    ): string;
+    hasEnoughMagic(skill: AttackSkillInterface): boolean;
+    insufficientMagicMessage(skill: AttackSkillInterface): string;
+    attackSuccessMessage(
+        skill: AttackSkillInterface,
+        attackedPokemon: AttackerPokemonInterface
+    ): string;
+    attackMessage(
+        skill: AttackSkillInterface,
+        attackedPokemon: AttackerPokemonInterface
+    ): string;
 }
