83a76ed007cc47a5d00ccdfcc58fbd7a42ca2529
mrbubbles-src <manuel.fahrenholz@digitalcareerinstitute.org>
2024-04-29 16:01:31 +0200

    chore(testing): fetch data
    implement code to test if datafetching works

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-// to be filled out :E
+// import getAbilityList from "./util/getAbilityList";
+// import getPokemonList from "./util/getPokemonList";
+
+// // to be filled out :E
+// const pokemonList = await getPokemonList();
+
+// // console.log(pokemonList);
+// // console.log(
+// //     pokemonList?.results.map((pkmn) => {
+// //         return pkmn.name.charAt(0).toUpperCase() + pkmn.name.slice(1);
+// //     })
+// // );
+
+// const abilityList = await getAbilityList();
+
+// // console.log(abilityList);
