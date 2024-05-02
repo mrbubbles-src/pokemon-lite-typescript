428e044cd5c4f3eb08b4b3b105395839eb454ffa
mrbubbles-src <manuel.fahrenholz@digitalcareerinstitute.org>
2024-05-02 12:53:04 +0200

    chore(testing): testing getPokemonDetails
    successfully got pokemon details needed

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,30 @@
 // import getAbilityList from "./util/getAbilityList";
-// import getPokemonList from "./util/getPokemonList";
+import getPokemonDetails from "./util/getPokemonDetails";
+import getPokemonList from "./util/getPokemonList";
+import { NamedAPIResourceList } from "pokenode-ts";
+// [
+//     { name: "voltorb", url: "https://pokeapi.co/api/v2/pokemon/101/" },
+// ]
+async function getAPkmn() {
+    const pokemonList: NamedAPIResourceList | undefined =
+        await getPokemonList();
+    console.log(pokemonList);
+    if (pokemonList === undefined) {
+        throw new Error("Pokemon couldn't be found");
+    }
+    const pokemonDetailsPromises = pokemonList.results.map(async (pokemon) => {
+        let pokeName = pokemon.name;
+        let pokeUrl = pokemon.url;
 
-// // to be filled out :E
-// const pokemonList = await getPokemonList();
+        const pkmn = await getPokemonDetails(pokeName, pokeUrl);
+        return pkmn;
+    });
+    const pokemonDetails = await Promise.all(pokemonDetailsPromises);
+    console.log(pokemonDetails);
+    return pokemonDetails;
+}
 
-// // console.log(pokemonList);
-// // console.log(
-// //     pokemonList?.results.map((pkmn) => {
-// //         return pkmn.name.charAt(0).toUpperCase() + pkmn.name.slice(1);
-// //     })
-// // );
-
+getAPkmn();
 // const abilityList = await getAbilityList();
 
 // // console.log(abilityList);
