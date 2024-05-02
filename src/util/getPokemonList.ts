e030807be58895d761d77873adbf514b67f47f93
mrbubbles-src <manuel.fahrenholz@digitalcareerinstitute.org>
2024-05-02 12:53:58 +0200

    chore(reduced incoming data): reduced pokemon fetched
    reduced amount of pokemon fetched from all to first 151

--- src/util/getPokemonList.ts
+++ src/util/getPokemonList.ts
@@ -5,8 +5,14 @@ export default async function getPokemonList(): Promise<
 > {
     const api = new PokemonClient();
 
+    // try {
+    //     const data = await api.listPokemons(0, 1025);
+    //     return data;
+    // } catch (error) {
+    //     console.error(error);
+    // }
     try {
-        const data = await api.listPokemons(0, 1025);
+        const data = await api.listPokemons(0, 151);
         return data;
     } catch (error) {
         console.error(error);
